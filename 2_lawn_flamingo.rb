663bcbdfb8c33bc8711b9937a36f6eb75ae32eb4
Gerzer <gjacobycooper_16@speyerlegacyschool.org>
2015-04-12 10:29:08 -0400

    Make things work

--- 2_lawn_flamingo.rb
+++ 2_lawn_flamingo.rb
@@ -1,2 +1,16 @@
 ##create your LawnFlamingo class here
-
+class LawnFlamingo
+  attr_accessor :name
+  attr_accessor :weapon
+  attr_accessor :angry
+  def initialize
+    @angry = false
+  end
+  def taunt(gnome)
+    gnome.angry = true
+    puts "Your mother was a hamster and your father smelled of elderberries!"
+  end
+  def attack
+    puts "Flamingos attack!!!"
+  end
+end
